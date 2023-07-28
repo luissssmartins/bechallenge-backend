9e2f7b3b40ed9d05a51bb9751164570ff68ae98e
Luis Martins <chateadorn@gmail.com>
2023-07-28 20:24:03 -0300

    alterando sqlite -> postgresql

--- bechallenge_backend/settings.py
+++ bechallenge_backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bechallenge_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bechallenge',
+        'USER': 'app',
+        'PASSWORD': 'beanalyctic',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
