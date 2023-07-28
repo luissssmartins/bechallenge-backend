54456aa0adf41e37c74f23c7bf0cccd15d9c5165
Luis Martins <chateadorn@gmail.com>
2023-07-28 19:46:50 -0300

    arrumando rotas

--- bechallenge_backend/tasks/urls.py
+++ bechallenge_backend/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskListCreateView
+from .views import TaskListCreateView, TaskUpdateDeleteView
 
 urlpatterns = [
-    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('api/tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('api/tasks/<int:pk>', TaskUpdateDeleteView.as_view(), name='task-update-delete'),
 ]
