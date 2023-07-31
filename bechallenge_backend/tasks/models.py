762080cf313275e6278d6aaf8d38900f76ddf602
Luis Martins <chateadorn@gmail.com>
2023-07-30 22:03:59 -0300

    atualizando serializer

--- bechallenge_backend/tasks/models.py
+++ bechallenge_backend/tasks/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Task(models.Model):
 
     name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    status = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.name
--- bechallenge_backend/tasks/serializers.py
+++ bechallenge_backend/tasks/serializers.py
@@ -6,4 +6,4 @@ class TaskSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'status']
